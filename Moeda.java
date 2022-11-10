92907fe9fb932646aba21e88fa7cd3dc58e683d0
Allan-Figueiredo <115052566+Allan-Figueiredo@users.noreply.github.com>
2022-11-09 21:40:23 -0300

    Update Moeda.java

--- Moeda.java
+++ Moeda.java
@@ -2,9 +2,9 @@
 public abstract class Moeda {
 	
 	protected double valor;
-	//Informação da classe moeda
+
 	public abstract void info();
-	//Converter da classe moeda
+
 	public abstract double converter();
 
 }
