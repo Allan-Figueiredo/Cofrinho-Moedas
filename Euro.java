e437100911c5fdf52a1b66a3d086ba1fc204e33c
Allan-Figueiredo <115052566+Allan-Figueiredo@users.noreply.github.com>
2022-11-09 21:40:19 -0300

    Update Euro.java

--- Euro.java
+++ Euro.java
@@ -1,4 +1,4 @@
-// Utilizando herança Euro(classe filha) extende Moeda(Classe mãe)
+
 public class Euro extends Moeda {
 	
 	public Euro(double valorInicial) {
@@ -14,17 +14,17 @@ public class Euro extends Moeda {
 	}
 
 	public double converter() {
-		//Valor do Euro convertido em Real
+
 		return this.valor * 6.5;
 	}
-	//Compatativo de classes
+
 	@Override
 	public boolean equals(Object o){
 		if (this.getClass()!= o.getClass()) {
 			return false;
 		}
 		
-		//Cast
+
 		
 		Euro objetoEuro = (Euro) o;
 		
