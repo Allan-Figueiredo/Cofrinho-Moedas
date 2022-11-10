486c74adc24cb25c535f51bce8364c869c41e33e
Allan-Figueiredo <115052566+Allan-Figueiredo@users.noreply.github.com>
2022-11-09 21:40:14 -0300

    Update Cofrinho.java

--- Cofrinho.java
+++ Cofrinho.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Cofrinho {
-	//Minha lista de Moedas utilizando o ArrayList com isso o Cofrinho consegue trabalhar com as moedas
+
 	private ArrayList<Moeda> listaMoedas;
 		
 	
@@ -9,19 +9,19 @@ public class Cofrinho {
 		this.listaMoedas = new ArrayList<>();
 		
 	}
-	//Adicionar moeda ao cofre
+
 	public void adicionar(Moeda moeda){
 		this.listaMoedas.add(moeda);
 			
 	}
-	//Remover moeda co cofre (boolean)pra ter retorno
+
 	public boolean remover(Moeda moeda){
 		return this.listaMoedas.remove(moeda);
 			
 	}
 	
 	public void listagemMoedas(){
-		//Se listar as moedas e não tiver moedas no Cofrinho
+
 		if(this.listaMoedas.isEmpty()) {
 			System.out.println("Não existe moeda no cofrinho ");
 			return;
@@ -39,7 +39,7 @@ public class Cofrinho {
     	if (this.listaMoedas.isEmpty()) {
 	    return 0;
       }
-    //Valor acumulado começa com zero	
+	
     double valorAcomulado = 0;
     
     for (Moeda moeda : this.listaMoedas) {
